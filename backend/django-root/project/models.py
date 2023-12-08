6c86bfb250dd959eb611ab15f1427310d4dad69d
Yiwei Liu <liu02yiwei@gmail.com>
2023-12-08 00:51:07 +0000

    Project model.

--- backend/django-root/project/models.py
+++ backend/django-root/project/models.py
@@ -1,3 +1,17 @@
+import datetime
+
 from django.db import models
+from django.core.serializers.json import DjangoJSONEncoder
 
 # Create your models here.
+
+
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    sheet = models.JSONField(encoder=DjangoJSONEncoder)
+    params = models.JSONField(encoder=DjangoJSONEncoder, blank=True)
+    create_date = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=100, blank=True)
+    is_deleted = models.BooleanField(default=False)
