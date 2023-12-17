151ed523fbc28921b337e2cae336fef38ace4101
Yiwei Liu <liu02yiwei@gmail.com>
2023-12-17 05:33:35 +0000

    Set note pitch and duration.

--- frontend/src/components/toolbar/index.js
+++ frontend/src/components/toolbar/index.js
@@ -1,16 +1,49 @@
 import React from "react";
 import { useNotes } from "../../contexts/notesContext";
-import { generateVoice } from "../../utils/generateVoice";
+import { useParameters } from "../../contexts/paramsContext";
+import { pitchFrequency } from "../../utils/pitchFrequency";
+import { keyNameList } from "../../utils/pianoKeys";
 import "./index.css";
 
-function Toolbar() {
-  const { notes } = useNotes();
+function Toolbar({ isDialogVisible }) {
+  const { notes, updateNotes } = useNotes();
+  const { numerator, denominator, bpm} = useParameters();
 
-  const handleAddLyrics = () => {};
-  const handleGenerate = () => {
-    console.log(`notes in toolbar: ${notes}`);
-    generateVoice(notes);
+  const handleEditLyrics = (flag) => {
+    isDialogVisible("isLyricsDialogVisible", flag)
   };
+
+  const handleGenerate = () => {
+    parsePitch();
+    parseDuration();
+    console.log(notes);
+    // call api
+  };
+
+  const parsePitch = () => {
+    let updatedNotes = [];
+    for (let i = 0; i < notes.length; i++) {
+      notes[i].octave = 8 - Math.floor((notes[i].startY / 25) / 12);
+      notes[i].keyName = keyNameList[11 - Math.floor((notes[i].startY / 25) % 12)];
+      notes[i].frequency = pitchFrequency[notes[i].octave][notes[i].keyName];
+      updatedNotes.push(notes[i]);
+    }
+    updateNotes(updatedNotes);
+  };
+
+  const parseDuration = () => {
+    const beatLength = 50; // TODO change dynamically, px
+    const beatDuration = 60 / bpm; // seconds
+    let updatedNotes = [];
+    for (let i = 0; i < notes.length; i++) {
+      let noteBeat = (notes[i].endX - notes[i].startX) / beatLength;
+      let noteDuration = noteBeat * beatDuration;
+      notes[i].duration = noteDuration;
+      updatedNotes.push(notes[i]);
+    }
+    updateNotes(updatedNotes);
+  };
+
   return (
     <div className="toolbar-wrapper">
       <span className="button add-lyrics-button" onClick={handleAddLyrics}>
