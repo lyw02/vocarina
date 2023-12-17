3c110d3bb3a0a799c354efa91fb6067b89d85305
Yiwei Liu <liu02yiwei@gmail.com>
2023-12-17 20:37:49 +0000

    Sticky piano.

--- frontend/src/components/pianoRoll/index.js
+++ frontend/src/components/pianoRoll/index.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import Piano from "../piano";
 import CanvasComponent from "../canvasComponent";
 import ComposeArea from "../composeArea";
 import "./index.css";
 
 function PianoRoll() {
+  const pianoRef = useRef(null);
+  const composeAreaRef = useRef(null);
   const pianoRollRef = useRef(null);
 
   useEffect(() => {
@@ -15,10 +17,13 @@ function PianoRoll() {
 
   return (
     <div className="piano-roll-wrapper" ref={pianoRollRef}>
-      <div className="piano-wrapper">
+      <div className="piano-wrapper" ref={pianoRef}>
         <Piano />
       </div>
-      <div className="compose-area-and-canvas-component-wrapper">
+      <div
+        className="compose-area-and-canvas-component-wrapper"
+        ref={composeAreaRef}
+      >
         <div className="compose-area-wrapper">
           <ComposeArea />
         </div>
