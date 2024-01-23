eb388f957f10f52cad64b9468720b75c52b86474
Yiwei Liu <liu02yiwei@gmail.com>
2024-01-23 03:08:06 +0000

    Material UI theme palette

--- frontend-ts/src/main.tsx
+++ frontend-ts/src/main.tsx
@@ -2,6 +2,15 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
 
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme.tsx";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <ThemeProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ThemeProvider>
 );
--- frontend-ts/src/pages/ProducePage/index.tsx
+++ frontend-ts/src/pages/ProducePage/index.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@mui/material";
+
 const ProducePage = () => {
-  return <div>ProducePage</div>;
+  return <div>ProducePage<Button variant="contained">Primary</Button></div>;
 };
 
 export default ProducePage;
