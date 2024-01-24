f81ff6abf22e22308ec8c1e31e0e4c727b172f7e
Yiwei Liu <liu02yiwei@gmail.com>
2024-01-24 21:55:18 +0000

    Community home page basic structure

--- frontend-ts/src/pages/CommunityHomePage/index.tsx
+++ frontend-ts/src/pages/CommunityHomePage/index.tsx
@@ -1,5 +1,20 @@
+import { Card, Grid } from "@mui/material";
+import { container, card } from "./style";
+
 const CommunityHomePage = () => {
-  return <div>CommunityHomePage</div>;
+  return (
+    <Grid container spacing={2} sx={container}>
+      <Grid item xs={3}>
+        <Card sx={card}>123</Card>
+      </Grid>
+      <Grid item xs={9}>
+        <Card sx={[card, { height: "100%" }]}>456</Card>
+      </Grid>
+      <Grid item xs={3}>
+        <Card sx={[card]}>123</Card>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CommunityHomePage;
---
+++ frontend-ts/src/pages/CommunityHomePage/style.ts
@@ -0,0 +1,11 @@
+import { SxProps } from "@mui/system";
+
+const container: SxProps = {
+  marginTop: "5px",
+} as const;
+
+const card = {
+  padding: "10px",
+} as const;
+
+export { container, card };
