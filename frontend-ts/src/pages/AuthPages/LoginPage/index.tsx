36e23c8a0125ce3cb839b99cfc15fe5c9e3da726
Yiwei Liu <liu02yiwei@gmail.com>
2024-02-04 01:40:53 +0000

    Auth pages

--- frontend-ts/src/pages/AuthPages/LoginPage/index.tsx
+++ frontend-ts/src/pages/AuthPages/LoginPage/index.tsx
@@ -1,3 +1,5 @@
+import AutoDismissAlert from "@/components/Alert/AutoDismissAlert";
+import theme from "@/theme";
 import {
   Button,
   Card,
@@ -10,7 +12,13 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
+
+interface Status {
+  severity: "success" | "error" | "warning" | "info";
+  message: string;
+}
 
 const linkStyle: SxProps = {
   textDecoration: "none",
@@ -19,6 +27,41 @@ const linkStyle: SxProps = {
 };
 
 const LoginPage = () => {
+  const [isAlertOpen, setIsAlertOpen] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [status, setStatus] = useState<Status>({
+    severity: "error",
+    message: "Login failed!",
+  });
+  const [promptMessage, setPromptMessage] = useState<string | null>();
+
+  const handleAlertClose = () => {
+    setIsAlertOpen(false);
+  };
+
+  const handleLogin = () => {
+    try {
+      if (!(username && password)) {
+        setPromptMessage("Please fill in all fields.");
+      } else {
+        // Send login request
+        setPromptMessage(null);
+        setStatus({
+          severity: "success",
+          message: "Login successed!",
+        });
+        setIsAlertOpen(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setStatus({
+        severity: "error",
+        message: "Login failed!",
+      });
+    }
+  };
+
   return (
     <div
       style={{
@@ -28,6 +71,12 @@ const LoginPage = () => {
         height: "100vh",
       }}
     >
+      <AutoDismissAlert
+        isAlertOpen={isAlertOpen}
+        handleAlertClose={handleAlertClose}
+        message={status.message}
+        severity={status.severity}
+      />
       <Card sx={{ maxWidth: 345, margin: "auto" }}>
         {/* <CardMedia
         sx={{ height: 140 }}
@@ -44,31 +93,42 @@ const LoginPage = () => {
               id="username-field"
               label="Username"
               variant="standard"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <TextField
               required
               id="password-field"
               label="Password"
               variant="standard"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <TextField
-              required
-              id="password-confirmation-field"
-              label="Password Confirmation"
-              variant="standard"
-            />
+            <Typography
+              gutterBottom
+              variant="caption"
+              component="span"
+              color={theme.palette.primary.main}
+            >
+              {promptMessage}
+            </Typography>
             <Stack direction="row" justifyContent="space-between">
-              <Typography gutterBottom variant="caption" component="span">
-                <Link sx={linkStyle} component={RouterLink} to="/register">Sign up</Link>
+              <Typography gutterBottom variant="overline" component="span">
+                <Link sx={linkStyle} component={RouterLink} to="/register">
+                  Sign up
+                </Link>
               </Typography>
-              <Typography gutterBottom variant="caption" component="span">
+              <Typography gutterBottom variant="overline" component="span">
                 <Link sx={linkStyle}>Forget password</Link>
               </Typography>
             </Stack>
           </Stack>
         </CardContent>
         <CardActions>
-          <Button size="small">Login</Button>
+          <Button size="small" onClick={handleLogin}>
+            Login
+          </Button>
         </CardActions>
       </Card>
     </div>
--- frontend-ts/src/pages/AuthPages/RegisterPage/index.tsx
+++ frontend-ts/src/pages/AuthPages/RegisterPage/index.tsx
@@ -1,3 +1,5 @@
+import AutoDismissAlert from "@/components/Alert/AutoDismissAlert";
+import theme from "@/theme";
 import {
   Button,
   Card,
@@ -10,15 +12,65 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
+
+interface Status {
+  severity: "success" | "error" | "warning" | "info";
+  message: string;
+}
 
 const linkStyle: SxProps = {
   textDecoration: "none",
-  userSelect: "none",
   cursor: "pointer",
 };
 
 const RegisterPage = () => {
+  const [isAlertOpen, setIsAlertOpen] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
+  const [status, setStatus] = useState<Status>({
+    severity: "error",
+    message: "Register failed!",
+  });
+  const [promptMessage, setPromptMessage] = useState<string | null>();
+
+  const usernameReg = /^[a-zA-Z0-9_\u4e00-\u9fa5]{4,20}$/;
+  const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,20}$/;
+
+  const handleAlertClose = () => {
+    setIsAlertOpen(false);
+  };
+
+  const handleSignUp = () => {
+    try {
+      if (!(username && password && passwordConfirmation)) {
+        setPromptMessage("Please fill in all fields.");
+      } else if (password !== passwordConfirmation) {
+        setPromptMessage("Passwords do not match.");
+      } else if (!passwordReg.test(password)) {
+        setPromptMessage("Password is not strong enough.");
+      } else if (!usernameReg.test(username)) {
+        setPromptMessage("Username is not valid.");
+      } else {
+        // Send sign up request
+        setPromptMessage(null);
+        setStatus({
+          severity: "success",
+          message: "Register successed!",
+        });
+        setIsAlertOpen(true);
+      }
+    } catch (error) {
+      console.log(error);
+      setStatus({
+        severity: "error",
+        message: "Register failed!",
+      });
+    }
+  };
+
   return (
     <div
       style={{
@@ -28,13 +80,19 @@ const RegisterPage = () => {
         height: "100vh",
       }}
     >
+      <AutoDismissAlert
+        isAlertOpen={isAlertOpen}
+        handleAlertClose={handleAlertClose}
+        message={status.message}
+        severity={status.severity}
+      />
       <Card sx={{ maxWidth: 345, margin: "auto" }}>
         {/* <CardMedia
           sx={{ height: 140 }}
           image="/static/images/cards/contemplative-reptile.jpg"
           title="green iguana"
         /> */}
-        <CardContent>
+        <CardContent sx={{ paddingBottom: 0 }}>
           <Typography gutterBottom variant="h5" component="div">
             Sign Up
           </Typography>
@@ -44,15 +102,43 @@ const RegisterPage = () => {
               id="username-field"
               label="Username"
               variant="standard"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <TextField
               required
               id="password-field"
               label="Password"
               variant="standard"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
+            <TextField
+              required
+              id="password-confirmation-field"
+              label="Password Confirmation"
+              variant="standard"
+              type="password"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+            />
+            <Typography
+              gutterBottom
+              variant="caption"
+              color={theme.palette.primary.main}
+              component="span"
+            >
+              {promptMessage}
+            </Typography>
             <Stack direction="row" justifyContent="space-between">
-              <Typography gutterBottom variant="caption" component="span">
+              <Typography
+                gutterBottom
+                variant="overline"
+                component="span"
+                sx={{ marginTop: "auto", paddingBottom: 0 }}
+              >
+                Already have an account?{" "}
                 <Link sx={linkStyle} component={RouterLink} to="/login">
                   Login
                 </Link>
@@ -60,8 +146,10 @@ const RegisterPage = () => {
             </Stack>
           </Stack>
         </CardContent>
-        <CardActions>
-          <Button size="small">Sign Up</Button>
+        <CardActions sx={{ paddingTop: 0 }}>
+          <Button size="small" onClick={handleSignUp}>
+            Sign Up
+          </Button>
         </CardActions>
       </Card>
     </div>
