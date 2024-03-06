f200c0e94cb0f550975f5adc3b1a3b1de4170860
Yiwei Liu <2814824250@qq.com>
2024-03-06 06:51:31 +0000

    Add more buttons.

--- frontend-ts/src/components/ParameterBar/index.tsx
+++ frontend-ts/src/components/ParameterBar/index.tsx
@@ -3,6 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/types";
 import InputDialog from "../InputDialog";
 import { setCurrentTrack } from "@/store/modules/tracks";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
 import "./index.css";
 
 const ParameterBar = () => {
@@ -25,24 +29,49 @@ const ParameterBar = () => {
     (state: RootState) => state.params
   );
 
+  const [value, setValue] = useState(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
   return (
     <div className="param-wrapper">
-      <span
-        className="param-item"
-        onClick={() => setIsTimeSigDialogVisible(true)}
-      >
-        {numerator}/{denominator}
-      </span>
-      <span className="param-item" onClick={() => setIsBpmDialogVisible(true)}>
-        BPM: {bpm}
-      </span>
-      <span>
-        <select value={currentTrack} onChange={handleTrackChange}>
-          {tracks.map((track) => (
-            <option key={track.trackId} value={track.trackId}>{track.trackName}</option>
-          ))}
-        </select>
-      </span>
+      <Stack justifyContent="space-between" direction="row" sx={{mb: 1}}>
+        <Stack alignItems="center" spacing={2} direction="row">
+          <span
+            className="param-item"
+            onClick={() => setIsTimeSigDialogVisible(true)}
+          >
+            {numerator}/{denominator}
+          </span>
+          <span
+            className="param-item"
+            onClick={() => setIsBpmDialogVisible(true)}
+          >
+            BPM: {bpm}
+          </span>
+          <span>
+            <select value={currentTrack} onChange={handleTrackChange}>
+              {tracks.map((track) => (
+                <option key={track.trackId} value={track.trackId}>
+                  {track.trackName}
+                </option>
+              ))}
+            </select>
+          </span>
+          <span>
+            <select>
+              <option>Select Voice</option>
+            </select>
+          </span>
+        </Stack>
+        <Stack spacing={2} direction="row" sx={{ width: "15%" }} alignItems="center">
+          <VolumeDown fontSize="small" />
+          <Slider aria-label="Volume" size="small" value={value} onChange={handleChange} />
+          <VolumeUp fontSize="small" />
+        </Stack>
+      </Stack>
       <InputDialog
         title="Edit Time Signature"
         formType="EditTimeSignatureForm"
--- frontend-ts/src/components/Toolbar/index.tsx
+++ frontend-ts/src/components/Toolbar/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Button, Stack } from "@mui/material";
 import "./index.css";
 import LyricsDialog from "../InputDialog/LyricsDialog";
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 
 const Toolbar = () => {
   const [isLyricsDialogVisible, setIsLyricsDialogVisible] =
@@ -22,17 +24,32 @@ const Toolbar = () => {
       <span className="button generate-button" onClick={handleGenerate}>
         Generate
       </span> */}
-      <Stack spacing={2} direction="row">
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={() => handleEditLyrics()}
-        >
-          Edit Lyrics
-        </Button>
-        <Button variant="contained" size="small">
-          Generate
-        </Button>
+      <Stack justifyContent="space-between" direction="row">
+        <Stack spacing={2} direction="row">
+          <Button variant="outlined" size="small">
+            Save Project
+          </Button>
+          <Button variant="outlined" size="small">
+            Import Project
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            onClick={() => handleEditLyrics()}
+          >
+            <EditOutlinedIcon />
+            Edit Lyrics
+          </Button>
+          <Button variant="contained" size="small">
+            Generate
+          </Button>
+        </Stack>
+        <Stack direction="row">
+          <Button variant="contained" size="small">
+            <PlayCircleFilledIcon sx={{marginRight: "5px"}} />
+            Play
+          </Button>
+        </Stack>
       </Stack>
       <LyricsDialog
         isOpen={isLyricsDialogVisible}
