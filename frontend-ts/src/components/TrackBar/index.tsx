22b44777412fa658c43908bbe4038909ff69baae
Yiwei Liu <liu02yiwei@gmail.com>
2024-03-27 17:26:09 +0000

    Update track bar

--- frontend-ts/src/components/TrackBar/index.tsx
+++ frontend-ts/src/components/TrackBar/index.tsx
@@ -5,8 +5,9 @@ import {
   setTrackState,
 } from "@/store/modules/tracks";
 import { RootState } from "@/types";
-import { AddCircleOutline } from "@mui/icons-material";
+import { AddCircleOutline, MoreHoriz } from "@mui/icons-material";
 import {
+  Box,
   Button,
   MenuItem,
   Select,
@@ -14,11 +15,15 @@ import {
   Stack,
   ToggleButton,
   ToggleButtonGroup,
+  Typography,
+  styled,
+  toggleButtonGroupClasses,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./index.css";
 import { trackState } from "@/types/project";
+import theme from "@/theme";
 
 const TrackBar = () => {
   const dispatch = useDispatch();
@@ -28,16 +33,12 @@ const TrackBar = () => {
   );
   const tracks = useSelector((state: RootState) => state.tracks.tracks);
 
-  const handleTrackChange = (event: SelectChangeEvent) => {
-    dispatch(setCurrentTrack(event.target.value));
+  const handleTrackChange = (trackId: number) => {
+    dispatch(setCurrentTrack(trackId));
     dispatch(
       setSheet({ trackId: currentTrack, sheet: tracks[currentTrack - 1].sheet })
     );
   };
-
-  useEffect(() => {
-    setToggle(tracks[currentTrack - 1].trackState);
-  }, [currentTrack])
 
   const handleCreateTrack = () => {
     dispatch(createNewTrack({ trackName: "" }));
@@ -47,37 +48,119 @@ const TrackBar = () => {
   const [toggle, setToggle] = useState(tracks[currentTrack - 1].trackState);
 
   const handleToggleChange = (
-    event: React.MouseEvent<HTMLElement>,
-    newToggle: trackState
+    _event: React.MouseEvent<HTMLElement>,
+    newToggle: trackState,
+    trackId: number
   ) => {
-    dispatch(setTrackState({ trackId: currentTrack, newState: newToggle }));
+    dispatch(setTrackState({ trackId: trackId, newState: newToggle }));
     setToggle(newToggle);
   };
+
+  const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
+    [`& .${toggleButtonGroupClasses.grouped}`]: {
+      margin: theme.spacing(0.5),
+      border: 0,
+      borderRadius: theme.shape.borderRadius,
+      [`&.${toggleButtonGroupClasses.disabled}`]: {
+        border: 0,
+      },
+    },
+    [`& .${toggleButtonGroupClasses.middleButton},& .${toggleButtonGroupClasses.lastButton}`]:
+      {
+        marginLeft: -1,
+        borderLeft: "1px solid transparent",
+      },
+  }));
+
   return (
     <div className="trackbar-wrapper">
-      <Stack direction="row" spacing={2}>
-        <Select value={currentTrack.toString()} onChange={handleTrackChange}>
-          {tracks.map((track) => (
-            <MenuItem key={track.trackId} value={track.trackId}>
-              {track.trackName}
-            </MenuItem>
-          ))}
-        </Select>
-        <ToggleButtonGroup
-          color="primary"
-          value={toggle}
-          exclusive
-          size="small"
-          onChange={handleToggleChange}
-          aria-label="Muted or solo"
-        >
-          <ToggleButton value="muted">M</ToggleButton>
-          <ToggleButton value="solo">S</ToggleButton>
-        </ToggleButtonGroup>
-        <Button size="small" onClick={handleCreateTrack}>
-          <AddCircleOutline />
-        </Button>
-      </Stack>
+      {tracks.map((track) => {
+        return (
+          <Stack
+            key={track.trackId}
+            direction="row"
+            spacing={0}
+            sx={
+              track.trackId === currentTrack
+                ? {
+                    backgroundColor: theme.palette.grey[200],
+                  }
+                : {
+                    backgroundColor: theme.palette.grey[100],
+                  }
+            }
+          >
+            <Box
+              component="div"
+              sx={{
+                p: 0,
+                border: "0px dashed grey",
+                width: "20%",
+              }}
+            >
+              <Stack direction="row" spacing={0}>
+                <Box
+                  component="div"
+                  sx={
+                    track.trackId === currentTrack
+                      ? {
+                          p: 1,
+                          width: "5%",
+                          minHeight: "100%",
+                          backgroundColor: theme.palette.primary.light,
+                        }
+                      : {
+                          p: 1,
+                          width: "5%",
+                          minHeight: "100%",
+                        }
+                  }
+                ></Box>
+                <Box
+                  component="div"
+                  sx={{ p: 0, width: "50%", display: "flex", cursor: "pointer" }}
+                  onClick={() => handleTrackChange(track.trackId)}
+                >
+                  <Typography
+                    variant="body2"
+                    sx={{ margin: "auto", userSelect: "none" }}
+                  >
+                    {track.trackName}
+                  </Typography>
+                </Box>
+                <Box component="div" sx={{ p: 0, width: "30%", height: "50%" }}>
+                  <StyledToggleButtonGroup
+                    color="primary"
+                    value={track.trackState}
+                    exclusive
+                    size="small"
+                    onChange={(e, v) => handleToggleChange(e, v, track.trackId)}
+                    aria-label="Muted or solo"
+                  >
+                    <ToggleButton value="muted">M</ToggleButton>
+                    <ToggleButton value="solo">S</ToggleButton>
+                  </StyledToggleButtonGroup>
+                </Box>
+                <Box
+                  component="div"
+                  sx={{ p: 0, width: "20%", display: "flex" }}
+                >
+                  <Button
+                    onClick={handleCreateTrack}
+                    sx={{ margin: "auto", minWidth: "80%", width: "80%" }}
+                  >
+                    <MoreHoriz />
+                  </Button>
+                </Box>
+              </Stack>
+            </Box>
+            <Box
+              component="div"
+              sx={{ p: 2, borderLeft: "1px solid lightgrey", width: "80%" }}
+            ></Box>
+          </Stack>
+        );
+      })}
     </div>
   );
 };
